8f8c18a59470064bb5661848f65e1c7f87f2ef7d
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2025-09-02 21:44:52 -0400

    Trusting that Codex knows how to fix worker.sh

--- worker.sh
+++ worker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 # Railway sets REDIS_URL; eager mode should be false/unset in prod.
-exec celery -A tasks.celery worker --loglevel=INFO
+exec celery -A tasks_core:celery worker --loglevel=INFO -I functions
